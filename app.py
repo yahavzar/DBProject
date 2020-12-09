2ba8a4e402f658b8b72bb0cf1c5e31204d1dd519
Yahav <yahavzar171@gmail.com>
2020-12-09 18:19:39 +0200

    init

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+import urllib
+import json
+import zlib
+import time
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    url = "https://api.themoviedb.org/3/movie/550?api_key="
+    key = "d005091db9214b502565db95dea43fc7"
+    res=urllib.urlopen(url+key).read()
 
 if __name__ == '__main__':
     app.run()
