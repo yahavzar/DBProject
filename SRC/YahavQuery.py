45da351998acb33e87e2c9f000d66e89b24a08c6
Yahav <yahavzar171@gmail.com>
2020-12-24 11:18:27 +0200

    11

--- SRC/YahavQuery.py
+++ SRC/YahavQuery.py
@@ -127,3 +127,42 @@ def recommended_actor_by_genre(connectionObject):
     for row in rows:
         print(row[0],"\t",row[1],'\t',row[2],'\n')
     return rows
+
+def recommended_actor_by_genre(connectionObject):
+    ## (query 2)
+    GenreName=input("enter Genre Name: ")
+    voteavg=input("enter vote avg: ")
+    cursorObject = connectionObject.cursor()
+    sqlQuery = "select a.actorId, a.actorName , count(*) from Actors a, MoviesGenre mg, Movie m ," \
+               " ActorsMovie am , Genre g , (select Genre.genreId as id from Genre where" \
+               " genreName=%s) AS temp, (select g.genreId,g.genreName,avg(voteCount) as" \
+               " avgvotecount from Movie m , Genre g, MoviesGenre mg where m.apiId=mg.apiId " \
+               "and g.genreId=mg.genreId group by g.genreId ) as GenreAvgVoteCount where" \
+               " m.apiId=mg.apiId and a.actorId=am.actorId and am.filmId=m.apiId and g.genreName=%s " \
+               "and  temp.id=mg.genreId and  m.voteCount>GenreAvgVoteCount.avgvotecount and m.voteAvg>%s" \
+               " and temp.id= GenreAvgVoteCount.genreId group by a.actorId order by -count(*)"
+    values = (GenreName,GenreName,voteavg)
+    cursorObject.execute(sqlQuery,values)
+    rows = cursorObject.fetchall()
+    for row in rows:
+        print(row[0],"\t",row[1],'\t',row[2],'\n')
+    return rows
+
+def recommended_foreign_language(connectionObject):
+    ## (query 11)
+    voteavg = input("enter vote avg: ")
+    cursorObject = connectionObject.cursor()
+    sqlQuery = "select m.title , l.languageName from Movie m,Language l, (select m.langId as lang, avg(m.budget) " \
+               "as budget from Movie m, Movie m2 where m.langId = m2.langId and m.budget is not null group by" \
+               " m.langId) as budgetAvg , (select m.langId as lang, avg(m.revenue) as revenue from Movie m," \
+               " Movie m2 where m.langId = m2.langId and m.revenue is not null group by m.langId) as revenueAvg," \
+               "(select m.langId as langId,avg(m.voteCount) as avgVote from Movie m,Movie m2 where m.langId = m2.langId " \
+               "group by m.langId) as voteCountByLang where m.langId  = budgetAvg.lang and m.langId=revenueAvg.lang and " \
+               "m.budget >budgetAvg.budget and m.revenue > revenueAvg.revenue and m.langId <> 2 and voteCountByLang.langId=m.langId" \
+               "  and m.voteCount>voteCountByLang.avgVote and m.voteAvg>%s and l.languageId = m.langId"
+    values = ( voteavg)
+    cursorObject.execute(sqlQuery, values)
+    rows = cursorObject.fetchall()
+    for row in rows:
+        print(row[0], "\t", row[1], '\n')
+    return rows
