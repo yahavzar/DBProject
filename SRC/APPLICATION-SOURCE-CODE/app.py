8ca10ed363d41462fad4df322a20a4df2c3b2c12
Yahav <yahavzar171@gmail.com>
2020-12-30 09:48:10 +0200

    start working on server-side

---
+++ SRC/APPLICATION-SOURCE-CODE/app.py
@@ -0,0 +1,21 @@
+import pymysql
+
+from SRC import RetrieveData
+from SRC import Queries
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+        return render_template('login.html')
+
+if __name__ == '__main__':
+    #connectionObject = pymysql.connect(host="127.0.0.1", user="DbMysql03", password="DbMysql03", db="DbMysql03",port=3305)
+    app.run()
