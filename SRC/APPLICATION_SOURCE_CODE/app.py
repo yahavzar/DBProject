c988bc9bbf94d3751c81724e36dedb4121fa91fe
Yahav <yahavzar171@gmail.com>
2021-01-02 12:40:36 +0200

    change sql_executor, finish movie.html

--- SRC/APPLICATION_SOURCE_CODE/app.py
+++ SRC/APPLICATION_SOURCE_CODE/app.py
@@ -14,16 +14,10 @@ def index():
 
 @app.route('/search')
 def search_return_html():
-    connectionObject = pymysql.connect(host="127.0.0.1", user="DbMysql03", password="DbMysql03", db="DbMysql03",
-                                       port=3305)
     query = request.args.get('query')
-    cursorObject = connectionObject.cursor()
     sqlQuery = "select Movie.apiId from Movie where Movie.title=%s"
-    values = (query)
-    cursorObject.execute(sqlQuery, values)
-    rows = cursorObject.fetchall()
-    for row in rows:
-        apiId = row[0]
+    res = select(sqlQuery,query)
+    apiId=res['rows'][0][0]
     sqlQuery = "select distinct commonMovie.title  from (SELECT m2.apiId as id,m2.title as title, " \
                "count(*) as count FROM Movie as m, Movie as m2, Actors as a, ActorsMovie as am, " \
                "ActorsMovie as am2 WHERE m.apiId=%s AND am.filmId<>am2.filmId AND am.filmId=m.apiId" \
@@ -34,46 +28,29 @@ def search_return_html():
                "mg.apiId=m.apiId AND mg.genreId=g.genreId AND mg2.apiId=m2.apiId AND mg.genreId=mg2.genreId " \
                "GROUP BY m2.apiId,m2.title) as commonGenre , Movie m1 where commonMovie.count >4 and commonGenre.count>2" \
                " and m1.apiId=commonMovie.id and m1.apiId=commonGenre.id"
-    values = (apiId, apiId)
-    cursorObject.execute(sqlQuery, values)
-    rows = cursorObject.fetchall()
+    res = select(sqlQuery, [apiId,apiId])
     str = ""
-    for row in rows :
-        str =str+ " , "+row[0]
-    # with connector get to your mysql server and query the DB
-    # return the answer to number_of_songs var.
-    number_of_songs = 8 #should be retrieved from the DB
-    return render_template('searchResults.html', movie=str, query=query)
+    for row in res['rows']:
+        str = str + " , " + row[0]
 
-@app.route('/movies')
-def specific_movie_to_html():
-    connectionObject = pymysql.connect(host="127.0.0.1", user="DbMysql03", password="DbMysql03", db="DbMysql03",
-                                       port=3305)
-    query = request.args.get('query')
+    return render_template('searchResults.html',movie=str,query=query,)
 
-    cursorObject = connectionObject.cursor()
+@app.route('/specific_movie/<apiId>')
+def specific_movie_to_html(apiId):
     sqlQuery = "select apiId,title from Movie where apiId=%s "
-    cursorObject.execute(sqlQuery,query)
-    rows = cursorObject.fetchall()
-    for row in rows:
-        apiId = row[0]
-        title = row[1]
-        break
-
-    return render_template('movies.html', movie=apiId, query=title)
+    res = select(sqlQuery,apiId)
+    movie = res['rows'][0][1]
+    return render_template('specific_movie.html', apiId=apiId,movie=movie)
 
 
 @app.route('/movie')
 def movie_to_html():
-    connectionObject = pymysql.connect(host="127.0.0.1", user="DbMysql03", password="DbMysql03", db="DbMysql03",
-                                       port=3305)
-    cursorObject = connectionObject.cursor()
+
     sqlQuery = "select apiId,title from Movie  "
-    cursorObject.execute(sqlQuery)
-    rows = cursorObject.fetchall()
-    somedict = [{"ApiId":row[0],
-                "Title":  row[1]} for row in rows]
-    return render_template('movie.html', rows= json.dumps(somedict))
+    res = select(sqlQuery)
+    result = [{res['headers'][0]: row[0],
+                 res['headers'][1]: row[1]} for row in res['rows']]
+    return render_template('movie.html', res=json.dumps(result))
 
 
 
@@ -81,5 +58,4 @@ def movie_to_html():
 
 
 if __name__ == '__main__':
-   #app.run()
-   select("SELECT * FROM Movie")
+   app.run()
