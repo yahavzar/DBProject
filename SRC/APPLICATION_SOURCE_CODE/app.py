c9918ab471b5696f59351e38c8b046a507f53feb
Yahav <yahavzar171@gmail.com>
2021-01-03 18:50:51 +0200

    do search in all pages for movies and to tv show

--- SRC/APPLICATION_SOURCE_CODE/app.py
+++ SRC/APPLICATION_SOURCE_CODE/app.py
@@ -1,13 +1,18 @@
 import json
+
 import pymysql
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,abort
 import cgi
+
+from SRC.APPLICATION_SOURCE_CODE.DB import sql_executor
 from SRC.APPLICATION_SOURCE_CODE.DB.sql_executor import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/movie')
+@app.route('/TV-Show')
 def index():
 
     sqlQuery = "select  m.title , pm.image from Movie m, ( select avg(voteCount) as " \
@@ -28,6 +33,92 @@ def index():
 
     return render_template('Front-Page.html',image0=image0,image1=image1,image2=image2,image3=image3,image4=image4)
 
+
+
+@app.route("/Search-Movies-or-TV-Shows",methods=['POST','GET'])
+def search_full_text():
+   if request.method == 'POST':
+       title = request.form['title']
+   sqlQuery="select s.title from Shows as s where MATCH(s.title) AGAINST(%s) union select m.title from Movie as m where MATCH(m.title) AGAINST(%s)"
+   res=select(sqlQuery,[title,title])
+   result=[{res['headers'][0]: row[0] } for row in res['rows']]
+   return render_template('Search-Movies-or-TV-Shows.html',res=json.dumps(result))
+
+@app.route('/tvshow/<apiId>')
+def TV_Show(apiId):
+    try :
+        sqlQuery = "select title from Shows where apiId=%s"
+        resTitle = select(sqlQuery,apiId)
+        resultTitle = [{resTitle['headers'][0]: row[0]} for row in resTitle['rows']]
+        resultTitle=resultTitle[0]['title']
+        sqlQuery = "select overview from ShowOverview where ShowOverview.showId=%s"
+        resOverView= select(sqlQuery,apiId)
+        resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
+        resultOverview=resultOverview[0]['overview']
+        sqlQuery="select image from PosterShow where apiId=%s"
+        resimage = select(sqlQuery,apiId)
+        resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
+        resultimage=resultimage[0]['image']
+        return render_template('TV-Show.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage)
+    except sql_executor.NoResultsException:
+        abort(404)
+
+@app.route('/movie/<apiId>')
+def movie(apiId):
+    try :
+        sqlQuery = "select title from Movie where Movie.apiId=%s"
+        resTitle = select(sqlQuery,apiId)
+        resultTitle = [{resTitle['headers'][0]: row[0]} for row in resTitle['rows']]
+        resultTitle=resultTitle[0]['title']
+        sqlQuery = "select overview from MovieOverview where MovieOverview.filmId=%s"
+        resOverView= select(sqlQuery,apiId)
+        resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
+        resultOverview=resultOverview[0]['overview']
+        sqlQuery="select image from PosterMovie where apiId=%s"
+        resimage = select(sqlQuery,apiId)
+        resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
+        resultimage=resultimage[0]['image']
+        return render_template('Movie.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage)
+    except sql_executor.NoResultsException:
+        abort(404)
+
+
+@app.route('/search')
+def search_return_html():
+    try :
+        resultTitle = request.args.get('search')
+        sqlQuery = "select apiId from Movie where title=%s"
+        res = select(sqlQuery,resultTitle)
+        resultapi = [{res['headers'][0]: row[0]} for row in res['rows']]
+        apiId = resultapi[0]['apiId']
+        sqlQuery = "select overview from MovieOverview where MovieOverview.filmId=%s"
+        resOverView= select(sqlQuery,apiId)
+        resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
+        resultOverview=resultOverview[0]['overview']
+        sqlQuery="select image from PosterMovie where apiId=%s"
+        resimage = select(sqlQuery,apiId)
+        resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
+        resultimage=resultimage[0]['image']
+        return render_template('Movie.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage)
+    except sql_executor.NoResultsException:
+        try :
+            resultTitle = request.args.get('search')
+            sqlQuery = "select apiId from Shows where title=%s"
+            res = select(sqlQuery, resultTitle)
+            resultapi = [{res['headers'][0]: row[0]} for row in res['rows']]
+            apiId = resultapi[0]['apiId']
+            sqlQuery = "select overview from ShowOverview where ShowOverview.showId=%s"
+            resOverView= select(sqlQuery,apiId)
+            resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
+            resultOverview=resultOverview[0]['overview']
+            sqlQuery="select image from PosterShow where apiId=%s"
+            resimage = select(sqlQuery,apiId)
+            resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
+            resultimage=resultimage[0]['image']
+            return render_template('TV-Show.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage)
+        except sql_executor.NoResultsException:
+            abort(404)
+
 @app.route('/Credits')
 def Credits():
     return render_template('Credits.html')
@@ -39,53 +130,6 @@ def Actors():
 @app.route('/Foreign-Languages')
 def Foreign_Languages():
     return render_template('Foreign-Languages.html')
-
-@app.route('/Search-Movies-or-TV-Shows')
-def Search_Movies_or_TV_Shows():
-    return render_template('Search-Movies-or-TV-Shows.html')
-
-@app.route('/TV-Show')
-def TV_Show():
-    return render_template('TV-Show.html')
-
-
-@app.route('/movie/<apiId>')
-def movie(apiId):
-    sqlQuery = "select title from Movie where Movie.apiId=%s"
-    resTitle = select(sqlQuery,apiId)
-    resultTitle = [{resTitle['headers'][0]: row[0]} for row in resTitle['rows']]
-    resultTitle=resultTitle[0]['title']
-    sqlQuery = "select overview from MovieOverview where MovieOverview.filmId=%s"
-    resOverView= select(sqlQuery,apiId)
-    resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
-    resultOverview=resultOverview[0]['overview']
-    sqlQuery="select image from PosterMovie where apiId=%s"
-    resimage = select(sqlQuery,apiId)
-    resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
-    resultimage=resultimage[0]['image']
-    return render_template('Movie.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage)
-
-
-@app.route('/search')
-def search_return_html():
-    resultTitle = request.args.get('search')
-    sqlQuery = "select apiId from Movie where title=%s"
-    res = select(sqlQuery,resultTitle)
-    resultapi = [{res['headers'][0]: row[0]} for row in res['rows']]
-    apiId = resultapi[0]['apiId']
-    sqlQuery = "select overview from MovieOverview where MovieOverview.filmId=%s"
-    resOverView= select(sqlQuery,apiId)
-    resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
-    resultOverview=resultOverview[0]['overview']
-    sqlQuery="select image from PosterMovie where apiId=%s"
-    resimage = select(sqlQuery,apiId)
-    resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
-    resultimage=resultimage[0]['image']
-    return render_template('Movie.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage)
-
-
-
-
 @app.route('/testmoce')
 def movie_to_html():
     sqlQuery = "select apiId,title from Movie   "
@@ -94,14 +138,7 @@ def movie_to_html():
                  res['headers'][1]: row[1]} for row in res['rows']]
     return render_template('testmoce.html', res=json.dumps(result))
 
-@app.route("/Search-Movies-or-TV-Shows",methods=['POST','GET'])
-def fun():
-   if request.method == 'POST':
-       title = request.form['title']
-   sqlQuery="select s.title from Shows as s where MATCH(s.title) AGAINST(%s) union select m.title from Movie as m where MATCH(m.title) AGAINST(%s)"
-   res=select(sqlQuery,[title,title])
-   result=[{res['headers'][0]: row[0] } for row in res['rows']]
-   return render_template('Search-Movies-or-TV-Shows.html',res=json.dumps(result))
+
 
 if __name__ == '__main__':
    app.run()
