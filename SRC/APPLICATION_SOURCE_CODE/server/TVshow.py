38442665b343c845fee8143e18678161056af744
Yahav <yahavzar171@gmail.com>
2021-01-11 16:13:59 +0200

    make aux functions for movie.py and TVshow.py

--- SRC/APPLICATION_SOURCE_CODE/server/TVshow.py
+++ SRC/APPLICATION_SOURCE_CODE/server/TVshow.py
@@ -9,95 +9,133 @@ tv_page = Blueprint('tvshow', __name__, template_folder='templates')
 @tv_page.route('/tvshow/<apiId>')
 def TV_Show(apiId):
     try :
-        sqlQuery = "select title from Shows where apiId=%s"
-        resTitle = select(sqlQuery,apiId)
-        resultTitle = [{resTitle['headers'][0]: row[0]} for row in resTitle['rows']]
-        resultTitle=resultTitle[0]['title']
-        sqlQuery = "select overview from ShowOverview where ShowOverview.showId=%s"
-        resOverView= select(sqlQuery,apiId)
-        resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
-        resultOverview=resultOverview[0]['overview']
-        sqlQuery="select image from PosterShow where apiId=%s"
-        resimage = select(sqlQuery,apiId)
-        resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
-        resultimage=resultimage[0]['image']
-        sqlQuery = "select * from Shows where Shows.apiId=%s"
-        resofShows = select(sqlQuery,apiId)
-        length = [{resofShows['headers'][4]: row[4]} for row in resofShows['rows']]
-        length = length[0]['length']
-        webSite = [{resofShows['headers'][5]: row[5]} for row in resofShows['rows']]
-        webSite = webSite[0]['homePage']
-        if webSite != "":
-            webSite = "<b>WebSite</b>: " + webSite
-        vote = [{resofShows['headers'][9]: row[9]} for row in resofShows['rows']]
-        vote = vote[0]['voteAvg']
-        seasons = [{resofShows['headers'][10]: row[10]} for row in resofShows['rows']]
-        seasons = seasons[0]['seasons']
-        sqlQuery = "select p.producerName from ProducersShow ps , Producers p where ps.ShowId=%s and p.producerId=ps.producerId"
-        try :
-            ShowPrducer = select(sqlQuery, apiId)
-            producer = [{ShowPrducer['headers'][0]: row[0]} for row in ShowPrducer['rows']]
-            producer = producer[0]['producerName']
-            if producer is not None:
-                producer = "<b>Producer</b>: " + producer
-        except sql_executor.NoResultsException:
-                producer = ""
-        sqlQuery = "select a.actorName from ActorsShow am , Actors a where ShowId=%s and a.actorId=am.actorId"
-        try :
-            ShowsActors = select(sqlQuery, apiId)
-            result = [{ShowsActors['headers'][0]: row[0]} for row in ShowsActors['rows']]
-            credit = ''
-            first = True
-            for actor in result:
-                if first == False:
-                    credit = credit + "," + actor['actorName']
-                if first == True:
-                    credit = actor['actorName']
-                    first = False
-            if credit != None:
-                credit = "<b>Cast :</b> " + credit;
-            sqlQuery = "select l.LangName from LanguageShow lm ,Language l where lm.showId=%s and l.languageId=lm.languageId"
-            LanguagesShow = select(sqlQuery, apiId)
-            result = [{LanguagesShow['headers'][0]: row[0]} for row in LanguagesShow['rows']]
-            first = True;
-            showlang=""
-            for lang in result:
-                if first == False:
-                    showlang = showlang + "," + lang['LangName']
-                if first == True:
-                    showlang = lang['LangName']
-                    first = False
-            if showlang != None:
-                showlang = "<b>Spoken Language :</b> " + showlang;
-        except sql_executor.NoResultsException:
-                credit = ""
-        imagerc1 = ""
-        linkc1 = ""
-        imagers1 = ""
-        links1 = ""
+        resultTitle, length, webSite, vote, seasons = getBasicInfo(apiId)
     except sql_executor.NoResultsException:
         render_template("Error.html")
     try:
-        sqlQuery ="select distinct commonShow.id ,pm.image from (SELECT m2.apiId as  id,m2.title as title,  " \
-                  "count(*) as count FROM Shows as m, Shows as m2, Actors as a, ActorsShow as am, ActorsShow " \
-                  "as am2 WHERE m.apiId=%s AND am.showId<>am2.showId AND am.showId=m.apiId  AND am.actorId=a.actorId " \
-                  " AND am2.showId=m2.apiId AND am.actorId=am2.actorId AND  m.langId=m2.langId  GROUP BY m2.apiId,m2.title) " \
-                  "as commonShow  , (SELECT distinct m2.apiId as  id, m2.title as title, count(*) as count FROM Shows as m," \
-                  " Shows as m2, Genre as g  , ShowGenre as mg, ShowGenre as mg2 WHERE m.apiId=%s AND mg.apiId<>mg2.apiId AND " \
-                  " mg.apiId=m.apiId AND mg.genreId=g.genreId AND mg2.apiId=m2.apiId AND  mg.genreId=mg2.genreId  GROUP BY " \
-                  "m2.apiId,m2.title) as commonGenre , Shows  m1,PosterShow pm where commonShow.count >=1 and commonGenre.count>=1 " \
-                  " and m1.apiId=commonShow.id and m1.apiId=commonGenre.id and commonShow.id= pm.apiId    "
+        resultOverview = getOverView(apiId)
+    except sql_executor.NoResultsException:
+        resultOverview=""
+    try:
+        resultimage = getPoster(apiId)
+    except sql_executor.NoResultsException:
+        resultimage = "../static/noimage.png"
+    try:
+        producer = getProducer(apiId)
+    except sql_executor.NoResultsException:
+            producer=""
+    try:
+        credit = getCast(apiId)
+    except sql_executor.NoResultsException:
+            credit=""
+    try:
+        showlang=getSpokenLang(apiId)
+    except sql_executor.NoResultsException:
+        showlang=""
+    imagers1, links1 = getSimilarShow(apiId)
+    imagerc1, linkc1 = getComputeShow(apiId)
+    if imagerc1 == "":
+            imagerc1 = "./static/noimage.png"
+            linkc1 = apiId
+    else:
+            imagerc1 = "https://image.tmdb.org/t/p/w500/" + imagerc1
+    if imagers1 == "":
+            imagers1 = "../static/noimage.png"
+            links1 = apiId
+    else:
+        imagers1 = "https://image.tmdb.org/t/p/w500/" + imagers1
+    return render_template('TV-Show.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage,length=length,webSite=webSite,vote=vote,seasons=seasons,producer=producer,credit=credit
+                               ,imagers1=imagers1,links1=links1,imagerc1=imagerc1,linkc1=linkc1,showlang=showlang)
+
+
+def getOverView(apiId):
+    sqlQuery = "select overview from ShowOverview where ShowOverview.showId=%s"
+    resOverView = select(sqlQuery, apiId)
+    resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
+    resultOverview = resultOverview[0]['overview']
+    return  resultOverview
+def getPoster(apiId):
+    sqlQuery = "select image from PosterShow where apiId=%s"
+    resimage = select(sqlQuery, apiId)
+    resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
+    resultimage = resultimage[0]['image']
+    return  resultimage;
+
+def getProducer(apiId):
+    sqlQuery = "select p.producerName from ProducersShow ps , Producers p where ps.ShowId=%s and p.producerId=ps.producerId"
+    ShowPrducer = select(sqlQuery, apiId)
+    producer = [{ShowPrducer['headers'][0]: row[0]} for row in ShowPrducer['rows']]
+    producer = producer[0]['producerName']
+    if producer is not None:
+        producer = "<b>Producer</b>: " + producer
+    else :
+        producer = ""
+    return producer
+
+def getCast(apiId):
+    sqlQuery = "select a.actorName from ActorsShow am , Actors a where ShowId=%s and a.actorId=am.actorId"
+    ShowsActors = select(sqlQuery, apiId)
+    result = [{ShowsActors['headers'][0]: row[0]} for row in ShowsActors['rows']]
+    credit = ''
+    first = True
+    for actor in result:
+        if first == False:
+            credit = credit + "," + actor['actorName']
+        if first == True:
+            credit = actor['actorName']
+            first = False
+    if credit != None:
+        credit = "<b>Cast :</b> " + credit;
+
+    return credit
+
+
+def getSpokenLang(apiId):
+    sqlQuery = "select l.LangName from LanguageShow lm ,Language l where lm.showId=%s and l.languageId=lm.languageId"
+    LanguagesShow = select(sqlQuery, apiId)
+    result = [{LanguagesShow['headers'][0]: row[0]} for row in LanguagesShow['rows']]
+    first = True;
+    showlang = ""
+    for lang in result:
+        if first == False:
+            showlang = showlang + "," + lang['LangName']
+        if first == True:
+            showlang = lang['LangName']
+            first = False
+    if showlang != None:
+        showlang = "<b>Spoken Language :</b> " + showlang;
+    else:
+        showlang=""
+    return showlang
+
+def getSimilarShow(apiId):
+    imagers1=""
+    links1=""
+    try :
+        sqlQuery = "select distinct commonShow.id ,pm.image from (SELECT m2.apiId as  id,m2.title as title,  " \
+               "count(*) as count FROM Shows as m, Shows as m2, Actors as a, ActorsShow as am, ActorsShow " \
+               "as am2 WHERE m.apiId=%s AND am.showId<>am2.showId AND am.showId=m.apiId  AND am.actorId=a.actorId " \
+               " AND am2.showId=m2.apiId AND am.actorId=am2.actorId AND  m.langId=m2.langId  GROUP BY m2.apiId,m2.title) " \
+               "as commonShow  , (SELECT distinct m2.apiId as  id, m2.title as title, count(*) as count FROM Shows as m," \
+               " Shows as m2, Genre as g  , ShowGenre as mg, ShowGenre as mg2 WHERE m.apiId=%s AND mg.apiId<>mg2.apiId AND " \
+               " mg.apiId=m.apiId AND mg.genreId=g.genreId AND mg2.apiId=m2.apiId AND  mg.genreId=mg2.genreId  GROUP BY " \
+               "m2.apiId,m2.title) as commonGenre , Shows  m1,PosterShow pm where commonShow.count >=1 and commonGenre.count>=1 " \
+               " and m1.apiId=commonShow.id and m1.apiId=commonGenre.id and commonShow.id= pm.apiId    "
         similarShow = select(sqlQuery, [apiId, apiId])
         resultS = [{similarShow['headers'][0]: row[0],
-                    similarShow['headers'][1]: row[1]} for row in similarShow['rows']]
+                similarShow['headers'][1]: row[1]} for row in similarShow['rows']]
         shuffle(resultS)
         imagers1 = resultS[0]['image']
-        if imagers1==None:
-            imagers1=""
         links1 = resultS[0]['id']
     except sql_executor.NoResultsException:
         pass
-    try:
+    return imagers1,links1
+
+
+def getComputeShow(apiId):
+    imagerc1=""
+    linkc1=""
+    try :
         sqlQuery = "select distinct m2.apiId ,pm.image from Shows m1,ShowGenre mg1 " \
                    ", Shows m2, ShowGenre  mg2 ,PosterShow pm where  m1.apiId=mg1.apiId" \
                    " and m2.releaseDay between m1.releaseDay - interval 6 month and m1.releaseDay " \
@@ -107,22 +145,27 @@ def TV_Show(apiId):
         resultM = [{commptiveShow['headers'][0]: row[0],
                     commptiveShow['headers'][1]: row[1]} for row in commptiveShow['rows']]
         shuffle(resultM)
-
         imagerc1 = resultM[0]['image']
         linkc1 = resultM[0]['apiId']
-        if imagerc1 == "":
-            imagerc1 = "./static/noimage.png"
-            linkc1 = apiId
-        else:
-            imagerc1 = "https://image.tmdb.org/t/p/w500/" + imagerc1
-        if imagers1 == "":
-            imagers1 = "../static/noimage.png"
-            links1 = apiId
-        else:
-            imagers1 = "https://image.tmdb.org/t/p/w500/" + imagers1
-
-        return render_template('TV-Show.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage,length=length,webSite=webSite,vote=vote,seasons=seasons,producer=producer,credit=credit
-                               ,imagers1=imagers1,links1=links1,imagerc1=imagerc1,linkc1=linkc1,showlang=showlang)
-
     except sql_executor.NoResultsException:
-        return render_template('TV-Show.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage,length=length,webSite=webSite,vote=vote,seasons=seasons,producer=producer,credit=credit,showlang=showlang)
+        pass
+    return imagerc1, linkc1
+
+def getBasicInfo(apiId):
+    sqlQuery = "select title from Shows where apiId=%s"
+    resTitle = select(sqlQuery, apiId)
+    resultTitle = [{resTitle['headers'][0]: row[0]} for row in resTitle['rows']]
+    resultTitle = resultTitle[0]['title']
+    sqlQuery = "select * from Shows where Shows.apiId=%s"
+    resofShows = select(sqlQuery, apiId)
+    length = [{resofShows['headers'][4]: row[4]} for row in resofShows['rows']]
+    length = length[0]['length']
+    webSite = [{resofShows['headers'][5]: row[5]} for row in resofShows['rows']]
+    webSite = webSite[0]['homePage']
+    if webSite != "":
+        webSite = "<b>WebSite</b>: " + webSite
+    vote = [{resofShows['headers'][9]: row[9]} for row in resofShows['rows']]
+    vote = vote[0]['voteAvg']
+    seasons = [{resofShows['headers'][10]: row[10]} for row in resofShows['rows']]
+    seasons = seasons[0]['seasons']
+    return resultTitle,length,webSite,vote,seasons
--- SRC/APPLICATION_SOURCE_CODE/server/movie.py
+++ SRC/APPLICATION_SOURCE_CODE/server/movie.py
@@ -10,77 +10,109 @@ movie_page = Blueprint('movie', __name__, template_folder='templates')
 @movie_page.route('/movie/<apiId>')
 def movie(apiId):
     try :
-        sqlQuery = "select * from Movie where Movie.apiId=%s"
-        resofMovie = select(sqlQuery,apiId)
-        resultTitle = [{resofMovie['headers'][1]: row[1]} for row in resofMovie['rows']]
-        resultTitle=resultTitle[0]['title']
-        sqlQuery = "select overview from MovieOverview where MovieOverview.filmId=%s"
-        resOverView= select(sqlQuery,apiId)
-        resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
-        resultOverview=resultOverview[0]['overview']
-        sqlQuery="select image from PosterMovie where apiId=%s"
-        resimage = select(sqlQuery,apiId)
-        resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
-        resultimage=resultimage[0]['image']
-        imdb = [{resofMovie['headers'][8]: row[8]} for row in resofMovie['rows']]
-        resultimdb="https://www.imdb.com/title/"+imdb[0]['imdbId']
-        length = [{resofMovie['headers'][4]: row[4]} for row in resofMovie['rows']]
-        length=length[0]['length']
-        collection = [{resofMovie['headers'][7]: row[7]} for row in resofMovie['rows']]
-        collection=collection[0]['collection']
-        if collection is not None :
-            collection = "<b>Collection</b>: " + collection
-        else :
-            collection=""
-        webSite = [{resofMovie['headers'][9]: row[9]} for row in resofMovie['rows']]
-        webSite = webSite[0]['homePage']
-        if webSite != "":
-            webSite = "<b>WebSite</b>: " + webSite
-        vote = [{resofMovie['headers'][13]: row[13]} for row in resofMovie['rows']]
-        vote = vote[0]['voteAvg']
-        sqlQuery = "select d.directorName from DirectorsMovie dm , Directors d where dm.filmId=%s and d.directorId=dm.directorId"
-        MovieDirector= select(sqlQuery,apiId)
-        director = [{MovieDirector['headers'][0]: row[0]} for row in MovieDirector['rows']]
-        director = director[0]['directorName']
-        if director is not None :
-            director = "<b>Director</b>: " + director
-        else :
-            director=""
-        sqlQuery = "select a.actorName from ActorsMovie am , Actors a where filmId=%s and a.actorId=am.actorId"
-        MovieActors= select(sqlQuery,apiId)
-        result = [{MovieActors['headers'][0]: row[0]} for row in MovieActors['rows']]
-        credit=''
-        first= True
-        for actor in result:
-            if  first==False:
-                credit = credit +"," +actor['actorName']
-            if first ==True :
-                credit  = actor['actorName']
-                first=False
-        if credit != None:
-            credit = "<b>Cast :</b> "  + credit;
-        imagerc1 = ""
-        linkc1 = ""
-        imagers1 = ""
-        links1 = ""
-        movielang=""
-        sqlQuery="select l.LangName from LanguageMovie lm ,Language l where lm.movieId=%s and l.languageId=lm.languageId"
-        LanguagesMovie= select(sqlQuery,apiId)
-        result = [{LanguagesMovie['headers'][0]: row[0]} for row in LanguagesMovie['rows']]
-        first=True;
-        for lang in result:
-            if first == False:
-                movielang = movielang + "," + lang['LangName']
-            if first == True:
-                movielang =  lang['LangName']
-                first = False
-        if movielang != None:
-            movielang = "<b>Spoken Language :</b> " + movielang;
-
-
+        resultTitle, resultimdb, length, collection, webSite, vote=getBasicInfo(apiId)
     except sql_executor.NoResultsException:
         render_template("Error.html")
+    try:
+        resultOverview=getOverView(apiId)
+    except sql_executor.NoResultsException:
+        resultOverview=""
+    try:
+        credit = getCast(apiId)
+    except sql_executor.NoResultsException:
+        credit=""
+    try:
+        director = getDirector(apiId)
+    except sql_executor.NoResultsException:
+        director=""
+    try:
+        resultimage = getPoster(apiId)
+    except sql_executor.NoResultsException:
+        resultimage= "../static/noimage.png"
     try :
+        movielang = getSpokenLang(apiId)
+    except  sql_executor.NoResultsException:
+        movielang=""
+    imagers1,links1=getSimilarMovie(apiId)
+    imagerc1,linkc1= getComputeMovie(apiId)
+    if imagerc1=="":
+            imagerc1= "./static/noimage.png"
+            linkc1=apiId
+    else :
+            imagerc1= "https://image.tmdb.org/t/p/w500/" +imagerc1
+    if imagers1=="":
+            imagers1= "../static/noimage.png"
+            links1=apiId
+    else :
+            imagers1= "https://image.tmdb.org/t/p/w500/" +imagers1
+    return render_template('Movie.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage,resultimdb=resultimdb,length=length,collection=collection,webSite=webSite,vote=vote,director=director,credit=credit,
+                               imagers1=imagers1,links1=links1,imagerc1=imagerc1,linkc1=linkc1,movielang=movielang)
+
+
+def getOverView(apiId):
+    sqlQuery = "select overview from MovieOverview where MovieOverview.filmId=%s"
+    resOverView = select(sqlQuery, apiId)
+    resultOverview = [{resOverView['headers'][0]: row[0]} for row in resOverView['rows']]
+    resultOverview = resultOverview[0]['overview']
+    return resultOverview
+
+
+def getPoster(apiId):
+    sqlQuery = "select image from PosterMovie where apiId=%s"
+    resimage = select(sqlQuery, apiId)
+    resultimage = [{resimage['headers'][0]: row[0]} for row in resimage['rows']]
+    resultimage = resultimage[0]['image']
+    return resultimage
+
+
+def getDirector(apiId):
+    sqlQuery = "select d.directorName from DirectorsMovie dm , Directors d where dm.filmId=%s and d.directorId=dm.directorId"
+    MovieDirector = select(sqlQuery, apiId)
+    director = [{MovieDirector['headers'][0]: row[0]} for row in MovieDirector['rows']]
+    director = director[0]['directorName']
+    if director is not None:
+        director = "<b>Director</b>: " + director
+    else:
+        director = ""
+    return  director
+
+def getCast(apiId):
+    sqlQuery = "select a.actorName from ActorsMovie am , Actors a where filmId=%s and a.actorId=am.actorId"
+    MovieActors = select(sqlQuery, apiId)
+    result = [{MovieActors['headers'][0]: row[0]} for row in MovieActors['rows']]
+    credit = ''
+    first = True
+    for actor in result:
+        if first == False:
+            credit = credit + "," + actor['actorName']
+        if first == True:
+            credit = actor['actorName']
+            first = False
+    if credit != None:
+        credit = "<b>Cast :</b> " + credit
+    return  credit
+
+def getSpokenLang(apiId):
+    sqlQuery = "select l.LangName from LanguageMovie lm ,Language l where lm.movieId=%s and l.languageId=lm.languageId"
+    LanguagesMovie = select(sqlQuery, apiId)
+    result = [{LanguagesMovie['headers'][0]: row[0]} for row in LanguagesMovie['rows']]
+    first = True;
+    for lang in result:
+        if first == False:
+            movielang = movielang + "," + lang['LangName']
+        if first == True:
+            movielang = lang['LangName']
+            first = False
+    if movielang != None:
+        movielang = "<b>Spoken Language :</b> " + movielang;
+    else:
+        movielang=""
+    return  movielang
+
+def getSimilarMovie(apiId):
+    imagers1 = ""
+    links1=""
+    try:
         sqlQuery = "select distinct commonMovie.id ,pm.image from (SELECT m2.apiId as" \
                    " id,m2.title as title,  count(*) as count FROM Movie as m, Movie as " \
                    "m2, Actors as a, ActorsMovie as am, ActorsMovie as am2 WHERE m.apiId=%s" \
@@ -93,36 +125,53 @@ def movie(apiId):
                    " mg.genreId=mg2.genreId  GROUP BY m2.apiId,m2.title) as commonGenre , Movie" \
                    " m1,PosterMovie pm where commonMovie.count >3 and commonGenre.count>2    " \
                    "and m1.apiId=commonMovie.id and m1.apiId=commonGenre.id and commonMovie.id= pm.apiId   "
-        similarMovie = select(sqlQuery,[ apiId,apiId])
+        similarMovie = select(sqlQuery, [apiId, apiId])
         resultS = [{similarMovie['headers'][0]: row[0],
-                   similarMovie['headers'][1]: row[1]} for row in similarMovie['rows']]
+                    similarMovie['headers'][1]: row[1]} for row in similarMovie['rows']]
         shuffle(resultS)
         imagers1 = resultS[0]['image']
         links1 = resultS[0]['id']
     except sql_executor.NoResultsException:
         pass
-    try:
+    return imagers1,links1
+
+def getComputeMovie(apiId):
+    imagerc1=""
+    linkc1=""
+    try :
         sqlQuery = "select distinct m2.apiId ,pm.image from Movie m1,MoviesGenre mg1 , Movie m2, " \
                    "MoviesGenre     mg2 , PosterMovie pm where  m1.apiId=mg1.apiId and m2.releaseDay " \
                    "between m1.releaseDay - interval 6 month and m1.releaseDay  and m2.apiId=mg2.apiId " \
                    "and mg1.genreId=mg2.genreId and m2.langId= m1.langId and m1.apiId=%s and m1.apiId <>m2.apiId and pm.apiId=m2.apiId "
         commptiveMovie = select(sqlQuery, apiId)
         resultM = [{commptiveMovie['headers'][0]: row[0],
-                   commptiveMovie['headers'][1]: row[1]} for row in commptiveMovie['rows']]
+                    commptiveMovie['headers'][1]: row[1]} for row in commptiveMovie['rows']]
         shuffle(resultM)
         imagerc1 = resultM[0]['image']
         linkc1 = resultM[0]['apiId']
-        if imagerc1=="":
-            imagerc1= "./static/noimage.png"
-            linkc1=apiId
-        else :
-            imagerc1= "https://image.tmdb.org/t/p/w500/" +imagerc1
-        if imagers1=="":
-            imagers1= "../static/noimage.png"
-            links1=apiId
-        else :
-            imagers1= "https://image.tmdb.org/t/p/w500/" +imagers1
-        return render_template('Movie.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage,resultimdb=resultimdb,length=length,collection=collection,webSite=webSite,vote=vote,director=director,credit=credit,
-                               imagers1=imagers1,links1=links1,imagerc1=imagerc1,linkc1=linkc1,movielang=movielang)
     except sql_executor.NoResultsException:
-        return render_template('Movie.html',resultTitle=resultTitle,resultOverview=resultOverview,resimage=resultimage,resultimdb=resultimdb,length=length,collection=collection,webSite=webSite,vote=vote,director=director,credit=credit,movielang=movielang)
+        pass
+    return imagerc1, linkc1
+
+def getBasicInfo(apiId):
+    sqlQuery = "select * from Movie where Movie.apiId=%s"
+    resofMovie = select(sqlQuery, apiId)
+    resultTitle = [{resofMovie['headers'][1]: row[1]} for row in resofMovie['rows']]
+    resultTitle = resultTitle[0]['title']
+    imdb = [{resofMovie['headers'][8]: row[8]} for row in resofMovie['rows']]
+    resultimdb = "https://www.imdb.com/title/" + imdb[0]['imdbId']
+    length = [{resofMovie['headers'][4]: row[4]} for row in resofMovie['rows']]
+    length = length[0]['length']
+    collection = [{resofMovie['headers'][7]: row[7]} for row in resofMovie['rows']]
+    collection = collection[0]['collection']
+    if collection is not None:
+        collection = "<b>Collection</b>: " + collection
+    else:
+        collection = ""
+    webSite = [{resofMovie['headers'][9]: row[9]} for row in resofMovie['rows']]
+    webSite = webSite[0]['homePage']
+    if webSite != "":
+        webSite = "<b>WebSite</b>: " + webSite
+    vote = [{resofMovie['headers'][13]: row[13]} for row in resofMovie['rows']]
+    vote = vote[0]['voteAvg']
+    return resultTitle,resultimdb,length,collection,webSite,vote;
