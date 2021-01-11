b7f6d123215d8fbb293f95c82cbd16d3e4658261
Yahav <yahavzar171@gmail.com>
2021-01-11 17:27:53 +0200

    fix Show.py

--- SRC/API_DATA_RETRIEVE/Show.py
+++ SRC/API_DATA_RETRIEVE/Show.py
@@ -19,24 +19,3 @@ class Show(Media):
     self.creators=creators
 
 
-
-  # def __repr__(self):
-  #   x = str(self.adult) + "\n" + \
-  #       str(self.collection) + "\n" + \
-  #       str(self.budget) + "\n" + \
-  #       str(self.genre) + "\n" + \
-  #       str(self.homepage) + "\n" + \
-  #       str(self.api_id) + "\n" + \
-  #       str(self.imdb_id) + "\n" + \
-  #       str(self.original_language) + "\n" + \
-  #       str(self.title) + "\n" + \
-  #       str(self.overview) + "\n" + \
-  #       str(self.popularity) + "\n" + \
-  #       str(self.release_date) + "\n" + \
-  #       str(self.revenue) + "\n" + \
-  #       str(self.runtime) + "\n" + \
-  #       str(self.spoken_languages) + "\n" + \
-  #       str(self.status) + "\n" + \
-  #       str(self.vote_count) + "\n" + \
-  #       str(self.vote_avg) + "\n"
-  #   return x
